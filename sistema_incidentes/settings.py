26c0974e4716835bf4abe102f6d30d0693f2cbb4
Cristian <cristian150697@hotmail.com>
2025-11-25 20:04:18 -0300

    Css e CRSF Adjust

--- sistema_incidentes/settings.py
+++ sistema_incidentes/settings.py
@@ -147,7 +147,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://stempreal.crionsolutions.com.br",
+    "https://www.stempreal.crionsolutions.com.br",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
